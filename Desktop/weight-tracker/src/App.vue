93e16b5b6469d1751e70e6705657715d09d6d50d
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-30 12:04:02 +0100

    set necessary variables

--- Desktop/weight-tracker/src/App.vue
+++ Desktop/weight-tracker/src/App.vue
@@ -4,6 +4,19 @@
 
 <script>
 import {ref, shallowRef, computed, watch, nextTick} from 'vue';
+import Chart from "chart.js/auto"
+
+const weights = ref([])
+
+const weightChartEl = ref(null)
+
+const weightChart = shallowRef(null)
+
+const weightInput = re(60.0)
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || {weight: 0}
+})
 </script>
 
 <style>
