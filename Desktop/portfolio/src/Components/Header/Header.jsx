774682c6609d0ecf9f1333ce05db5c91ee0a7a26
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-13 20:27:38 +0100

    created header section

--- Desktop/portfolio/src/Components/Header/Header.jsx
+++ Desktop/portfolio/src/Components/Header/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import "./Header.css"
+import CTA from "./CTA"
+import MyImage from "../../assets/me.png"
+import HeaderSocials from "./headerSocials"
 
 const Header = () => {
   return (
-    <div>
-      <div>Header</div>
-    </div>
-  )
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>ToyoAbasi</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="myImage">
+          <img src={MyImage} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">To Bottom</a>
+      </div>
+    </header>
+    )
 }
 
 export default Header
