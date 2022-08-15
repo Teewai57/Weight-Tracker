c8453711658d0fc51dee8c44529127eb7f89daa3
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-15 21:16:58 +0100

    working on experience section

--- Desktop/portfolio/src/Components/Experience/Experience.jsx
+++ Desktop/portfolio/src/Components/Experience/Experience.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import "./Experience.css"
+import {BsPatchCheckFill} from "react-icons/bs"
+
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>BOOTSTRAP</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>JAVASCRIPT</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>VUE.JS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>REACT.JS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>VERSION CONTROL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill />
+              <h4>TAILWIND CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
