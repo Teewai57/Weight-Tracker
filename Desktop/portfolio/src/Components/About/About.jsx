943ad1f5c5ea782e89f6e9210cfa08f3d2f5f12a
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-15 11:40:26 +0100

    created about image

--- Desktop/portfolio/src/Components/About/About.jsx
+++ Desktop/portfolio/src/Components/About/About.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import "./About.css";
+import MeImage from "../../assets/me-about.jpg"
+import {FaAward} from "react-icons/fa"
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
+
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+            <div className="about_me-image">
+              <img src={MeImage} alt="About Image" />
+            </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>9+ months Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>10+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione aut dolorem placeat, 
+            nulla sed eveniet quos. Quos porro nihil ratione aut ipsum, non, quod, suscipit obcaecati
+            tenetur deleniti perspiciatis consequuntur.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          
+        </div>
+      </div>
     </section>
   )
 }
