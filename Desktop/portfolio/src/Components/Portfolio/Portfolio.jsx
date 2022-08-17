ba8d0f5e5f2c04340f05dd2f4d1df59ee11dd5ca
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-17 21:47:43 +0100

    started portfolio section

--- Desktop/portfolio/src/Components/Portfolio/Portfolio.jsx
+++ Desktop/portfolio/src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import "./Portfolio.css"
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+
 const Portfolio = () => {
   return (
-    <section>
-      <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+              <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/" className='btn'>Github</a>
+          <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
     </section>
   )
 }
