8fcb56bcd380f68127fbe7cefbafbb9f09392821
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-17 22:22:22 +0100

    creating testimonial section

--- Desktop/portfolio/src/Components/Testimonials/Testimonials.jsx
+++ Desktop/portfolio/src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./Testimonials.css"
+import AVTR1 from "../../assets/avatar1.jpg"
+import AVTR2 from "../../assets/avatar2.jpg"
+import AVTR3 from "../../assets/avatar3.jpg"
+import AVTR4 from "../../assets/avatar4.jpg"
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      Testimonials
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5>Ernest Willer</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci nostrum aperiam a accusantium natus dolore modi, repellendus ullam quibusdam magni nobis quo reiciendis possimus autem voluptatibus similique voluptas dicta!
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5>Ernest Willer</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci nostrum aperiam a accusantium natus dolore modi, repellendus ullam quibusdam magni nobis quo reiciendis possimus autem voluptatibus similique voluptas dicta!
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar one" />
+            <h5>Ernest Willer</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci nostrum aperiam a accusantium natus dolore modi, repellendus ullam quibusdam magni nobis quo reiciendis possimus autem voluptatibus similique voluptas dicta!
+            </small>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
