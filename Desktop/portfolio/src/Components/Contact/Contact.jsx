742593853bd9adff3ad0e56bc0b77e7511500a0e
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-22 23:34:31 +0100

    creating contact

--- Desktop/portfolio/src/Components/Contact/Contact.jsx
+++ Desktop/portfolio/src/Components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./Contact.css";
 import {MdOutlineEmail} from "react-icons/md"
+import {FaLinkedin} from "react-icons/fa"
+import {BsWhatsapp} from "react-icons/bs"
 
 const Contact = () => {
   return (
@@ -14,19 +16,19 @@ const Contact = () => {
             <MdOutlineEmail/>
             <h4>Email</h4>
             <h5>bobtoyoabasi@gmail.com</h5>
-            <a href="mailto:bobtoyoabasi@gmail.com">Send a message</a>
+            <a href="mailto:bobtoyoabasi@gmail.com"  target="_blank">Send a message</a>
           </article>
           <article className='contact_option'>
-            <MdOutlineEmail/>
+            <FaLinkedin/>
             <h4>LinkedIn</h4>
             <h5>Toyoabasi Bob</h5>
-            <a href="mailto:bobtoyoabasi@gmail.com">Send a message</a>
+            <a href="https://ng.linkedin.com/in/toyoabasi-bob-9835a7245?trk=people-guest_people_search-card" target="_blank">Send a message</a>
           </article>
           <article className='contact_option'>
-            <MdOutlineEmail/>
-            <h4>Email</h4>
-            <h5>bobtoyoabasi@gmail.com</h5>
-            <a href="mailto:bobtoyoabasi@gmail.com">Send a message</a>
+            <BsWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5>+2348162340257</h5>
+            <a href="https://api.whatsapp.com/send?phonenumber+2348162340257" target="_blank">Send a message</a>
           </article>
         </div>
         <form action=""></form>
