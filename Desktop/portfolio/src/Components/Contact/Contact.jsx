2ddce909d1f54aae4b3b06c6dfce82f959ca6378
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-24 22:44:13 +0100

    installed emailjs

--- Desktop/portfolio/src/Components/Contact/Contact.jsx
+++ Desktop/portfolio/src/Components/Contact/Contact.jsx
@@ -3,8 +3,25 @@ import "./Contact.css";
 import {MdOutlineEmail} from "react-icons/md"
 import {FaLinkedin} from "react-icons/fa"
 import {BsWhatsapp} from "react-icons/bs"
+import { useRef } from 'react';
+import emailjs from "emailjs-com"
+
 
 const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ni73a0y', 'template-rnq7mcd', form.current, 'leMbOuOYgUGLHxo3dph3e')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -31,7 +48,7 @@ const Contact = () => {
             <a href="https://wa.me/08162340257" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Full name' required />
           <input type="email" name="email" placeholder='Email' required />
           <textarea name="message" rows="7" placeholder='Your message' required></textarea>
