4522cb311ebafcac51b847e4a3d9b6a8ef149258
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-25 14:21:54 +0100

    initiated footer

--- Desktop/portfolio/src/Components/Footer/Footer.jsx
+++ Desktop/portfolio/src/Components/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import "./Footer.css"
+import {MdOutlineEmail} from "react-icons/md"
+import {FaLinkedin} from "react-icons/fa"
+import {BsWhatsapp} from "react-icons/bs"
+
+
 const Footer = () => {
   return (
-    <section>
-      <div>Footer</div>
-    </section>
+    <footer>
+      <a href="#" className='footer_logo'>Toyo</a>
+
+      <ul className='premalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact"></a>Contact Me</li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://ng.linkedin.com/in/toyoabasi-bob-9835a7245?trk=people-guest_people_search-card" target='_blank'><FaLinkedin /></a>
+        <a href="https://wa.me/08162340257" target="_blank"><BsWhatsapp /></a>
+        <a href="mailto:bobtoyoabasi@gmail.com"  target="_blank"><MdOutlineEmail /></a>
+      </div>
+
+      <div className="footer_copyrights">
+        <small>&copy; Toyo. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
